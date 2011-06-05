e0d2c0b8c7255aa5b8f2a0c0811b6ca3b20ed6a9
Taejoon Kwon <taejoon.kwon@mail.utexas.edu>
2011-06-05 17:19:02 -0500

    Add OMSSA support.

--- bin/mstb-setup.py
+++ bin/mstb-setup.py
@@ -5,8 +5,8 @@ import time
 
 import mstb_helper as helper
 
-DIR_LIST = ['DB','RAW','mzXML','mgf','tmp','scripts']
-ENGINE_LIST = ['sequest','inspect','tandem','tandemK','omssa','myrimatch','MSGFDB']
+DIR_LIST = ['DB','RAW','mzXML','mgf','ms2','tmp','scripts']
+#ENGINE_LIST = ['sequest','inspect','tandem','tandemK','omssa','myrimatch','MSGFDB']
 
 MSTB_HOME = helper.get_mstb_home()
 CWD = helper.get_cwd()
@@ -26,7 +26,7 @@ cparams['DB_NAME'] = os.path.join(CWD,'DB','your_db')
 cparams['DB_FASTA'] = os.path.join(CWD,'DB','your_db.fa')
 cparams['DB_FASTAPRO'] = os.path.join(CWD,'DB','your_db.fa.pro')
 cparams['DB_TRIE'] = os.path.join(CWD,'DB','your_db.fa.trie')
-cparams['DB_FORMATDB'] = os.path.join(CWD,'DB','your_db.fa')
+cparams['DB_BLASTDB'] = os.path.join(CWD,'DB','your_db.fa')
 
 cparams['PATH_TPP'] = '/usr/local/tpp'
 cparams['PATH_MSCONVERT'] = '/usr/local/tpp/bin/msconvert'
@@ -36,7 +36,9 @@ cparams['PATH_TANDEMK_EXE'] = '/usr/local/tpp/bin/tandem.exe'
 cparams['PATH_TANDEM2XML'] = '/usr/local/tpp/bin/Tandem2XML'
 cparams['PATH_TANDEMK_DEFAULT_PARAM'] = '/usr/local/tpp/bin/isb_default_input_kscore.xml'
 
-cparams['PATH_INSPECT'] = '/usr/local/src/inspect/current/'
+cparams['PATH_OMSSACL'] = '/usr/local/bin/omssacl'
+
+cparams['PATH_INSPECT'] = '/usr/local/bin/inspect'
 cparams['PATH_MSGFDB_JAR'] = '/usr/local/src/MSGFDB/current.jar'
 
 for filename_db in os.listdir( os.path.join(CWD,'DB') ):
@@ -46,7 +48,7 @@ for filename_db in os.listdir( os.path.join(CWD,'DB') ):
     if( filename_db.endswith('.pro') ):
         cparams['DB_FASTAPRO'] = os.path.join(CWD,'DB',filename_db)
     if( filename_db.endswith('.psq') ):
-        cparams['DB_FORMATDB'] = os.path.join(CWD,'DB',filename_db.replace('.psq',''))
+        cparams['DB_BLASTDB'] = os.path.join(CWD,'DB',filename_db.replace('.psq',''))
     if( filename_db.endswith('.trie') ):
         cparams['DB_TRIE'] = os.path.join(CWD,'DB',filename_db)
 
@@ -60,7 +62,7 @@ if( os.access(filename_conf_dest, os.F_OK) ):
     f_conf_dest_old = open(filename_conf_dest_old,'r')
     for line in f_conf_dest_old:
         tokens = line.strip().split()
-        if( len(tokens) > 2 ):
+        if( len(tokens) > 1 ):
             cparams[tokens[0]] = tokens[1]
     f_conf_dest_old.close()
 
