b7dcc0b41790ba8ef0a869a9d025650ecafc8d41
Taejoon Kwon <taejoon.kwon@mail.utexas.edu>
2011-06-05 15:25:32 -0500

    Add parameters for X!Tandem (k-score)

--- bin/mstb-setup.py
+++ bin/mstb-setup.py
@@ -3,11 +3,13 @@ import os
 import sys
 import time
 
+import mstb_helper as helper
+
 DIR_LIST = ['DB','RAW','mzXML','mgf','tmp','scripts']
 ENGINE_LIST = ['sequest','inspect','tandem','tandemK','omssa','myrimatch','MSGFDB']
 
-MSTB_HOME = os.path.join(sys.path[0],'..')
-CWD = os.getcwd()
+MSTB_HOME = helper.get_mstb_home()
+CWD = helper.get_cwd()
 
 for tmp_dir in DIR_LIST:
     tmp_path = os.path.join(CWD,tmp_dir)
@@ -16,24 +18,33 @@ for tmp_dir in DIR_LIST:
     else:
         sys.stderr.write("Create %s.\n"%(tmp_path))
         os.mkdir(tmp_path)
+sys.stderr.write("\n")
 
 cparams = dict()
-cparams['PATH_TPP'] = '/usr/local/tpp'
-cparams['PATH_MSCONVERT'] = '/usr/local/bin/msconvert'
-cparams['PATH_INSPECT'] = '/usr/local/src/inspect/current/'
-cparams['PATH_MSGFDB_JAR'] = '/usr/local/src/MSGFDB/current.jar'
-cparams['PATH_FASTAPRO_EXE'] = '/usr/local/bin/fastapro.exe'
 
+cparams['DB_NAME'] = os.path.join(CWD,'DB','your_db')
 cparams['DB_FASTA'] = os.path.join(CWD,'DB','your_db.fa')
 cparams['DB_FASTAPRO'] = os.path.join(CWD,'DB','your_db.fa.pro')
 cparams['DB_TRIE'] = os.path.join(CWD,'DB','your_db.fa.trie')
 cparams['DB_FORMATDB'] = os.path.join(CWD,'DB','your_db.fa')
 
+cparams['PATH_TPP'] = '/usr/local/tpp'
+cparams['PATH_MSCONVERT'] = '/usr/local/tpp/bin/msconvert'
+
+cparams['PATH_FASTAPRO_EXE'] = '/usr/local/bin/fastapro.exe'
+cparams['PATH_TANDEMK_EXE'] = '/usr/local/tpp/bin/tandem.exe'
+cparams['PATH_TANDEM2XML'] = '/usr/local/tpp/bin/Tandem2XML'
+cparams['PATH_TANDEMK_DEFAULT_PARAM'] = '/usr/local/tpp/bin/isb_default_input_kscore.xml'
+
+cparams['PATH_INSPECT'] = '/usr/local/src/inspect/current/'
+cparams['PATH_MSGFDB_JAR'] = '/usr/local/src/MSGFDB/current.jar'
+
 for filename_db in os.listdir( os.path.join(CWD,'DB') ):
-    if( filename_db.endswith('.pro') ):
-        cparams['DB_FASTAPRO'] = os.path.join(CWD,'DB',filename_db)
     if( filename_db.endswith('.fa') or filename_db.endswith('.fasta') ):
         cparams['DB_FASTA'] = os.path.join(CWD,'DB',filename_db)
+        cparams['DB_NAME'] = filename_db.replace('.fa','').replace('.fasta','')
+    if( filename_db.endswith('.pro') ):
+        cparams['DB_FASTAPRO'] = os.path.join(CWD,'DB',filename_db)
     if( filename_db.endswith('.psq') ):
         cparams['DB_FORMATDB'] = os.path.join(CWD,'DB',filename_db.replace('.psq',''))
     if( filename_db.endswith('.trie') ):
@@ -44,7 +55,7 @@ filename_conf_dest = os.path.join(CWD,'mstb.conf')
 
 if( os.access(filename_conf_dest, os.F_OK) ):
     filename_conf_dest_old = os.path.join(CWD,'mstb.conf.%s'%(time.strftime("%Y%b%d_%H%M%S")))
-    sys.stderr.write('%s exists.\n===> %s\n'%(filename_conf_dest, filename_conf_dest_old))
+    sys.stderr.write('%s exists.\n===> %s\n\n'%(filename_conf_dest, filename_conf_dest_old))
     os.rename(filename_conf_dest, filename_conf_dest_old)
     f_conf_dest_old = open(filename_conf_dest_old,'r')
     for line in f_conf_dest_old:
@@ -57,6 +68,7 @@ f_conf_src = open(filename_conf_src,'r')
 conf_tmpl = ''.join( f_conf_src.readlines() )
 f_conf_src.close()
 
+sys.stderr.write('Write %s.\n\n'%filename_conf_dest)
 f_conf_dest = open(filename_conf_dest,'w')
 f_conf_dest.write(conf_tmpl.format(**cparams))
 f_conf_dest.close()
