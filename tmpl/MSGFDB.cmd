8824f4c789773a49124c9b8b7638b9d66be8f4a6
Blake Borgeson <git@blakeb.org>
2012-07-04 16:23:42 -0500

    MSGFDB and inspect will now only run the search on that mzXML file if the appropriate output file does not already exist.

--- tmpl/MSGFDB.cmd
+++ tmpl/MSGFDB.cmd
@@ -1,1 +1,1 @@
-java -Xmx8000M -jar {PATH_MSGFDB_JAR} -d {DB_FASTA} -s {FILENAME_MZXML} -t 30ppm -c13 1 -nnet 0 -n 2 -o {FILENAME_OUT}
+if ! [ -f {FILENAME_OUT} ]; then java -Xmx8000M -jar {PATH_MSGFDB_JAR} -d {DB_FASTA} -s {FILENAME_MZXML} -t 30ppm -c13 1 -nnet 0 -n 2 -o {FILENAME_OUT}; fi
