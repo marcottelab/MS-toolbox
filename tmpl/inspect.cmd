ce7620d2e31b436922612f364d887714513b9a9d
Blake Borgeson <git@blakeb.org>
2012-07-07 13:06:03 -0500

    Another attempt at inspect parallelization

--- tmpl/inspect.cmd
+++ tmpl/inspect.cmd
@@ -1,1 +1,1 @@
-if [ ! -f {FILENAME_OUT} ] && [ ! -f {FILENAME_OUT}.tmp ]; then {PATH_INSPECT} -r {DIR_INSPECT} -i {FILENAME_IN} -o {FILENAME_OUT}; fi
+if [ ! -f {FILENAME_OUT} ] && [ ! -f {FILENAME_OUT}.tmp ]; then touch {FILENAME_OUT}.inprog; {PATH_INSPECT} -r {DIR_INSPECT} -i {FILENAME_IN} -o {FILENAME_OUT}; fi
