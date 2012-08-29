d4d21270cab3b110317f1a195a090dc99b2ea7cd
Blake Borgeson <git@blakeb.org>
2012-08-29 16:50:50 -0500

    Added support for unlimited concurrent msgfdb runs via temporary lock file.

--- tmpl/MSGFDB.cmd
+++ tmpl/MSGFDB.cmd
@@ -1,1 +1,1 @@
-if [ ! -f {FILENAME_OUT} ] && mkdir {FILENAME_OUT}.tmp ; then java -Xmx8000M -jar {PATH_MSGFDB_JAR} -d {DB_FASTA} -s {FILENAME_MZXML} -t 30ppm -c13 1 -nnet 0 -n 2 -o {FILENAME_OUT}; rmdir {FILENAME_OUT}.tmpt ; fi
+if [ ! -f {FILENAME_OUT} ] && mkdir {FILENAME_OUT}.tmp ; then java -Xmx8000M -jar {PATH_MSGFDB_JAR} -d {DB_FASTA} -s {FILENAME_MZXML} -t 30ppm -c13 1 -nnet 0 -n 2 -o {FILENAME_OUT}; rmdir {FILENAME_OUT}.tmp ; fi
